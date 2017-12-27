b635a29dee3bb15ef4f7ff8d400ad61d1b022661
Rahul22Joy <rahul22joy@gmail.com>
2017-12-27 11:44:11 +1100

    newly updated

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var fs = require('fs');
-    var stream = fs.createWriteStream("my_file.txt");
+    var stream = fs.createWriteStream("/home/rahul/my_file.txt");
     stream.once('open', function(fd) {
       stream.write("My first row\n");
       stream.write("My second row\n");
